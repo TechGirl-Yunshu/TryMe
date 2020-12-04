4a6a06d4ac2b7500345540a12ef7f63b81709cdd
Gisele Xiao <yunshu0507@gmail.com>
2020-12-04 14:34:37 -0500

    click to change a random color

--- app/src/main/java/com/example/tryme/MainActivity.java
+++ app/src/main/java/com/example/tryme/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.tryme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View windowView;
+    private Button tryMeButton;
+    private int[] colors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        colors = new int[]{Color.GRAY, Color.GREEN, Color.YELLOW, Color.RED, Color.BLUE, Color.LTGRAY, Color.MAGENTA, Color.CYAN};
+
+        windowView = findViewById(R.id.windowViewId);
+
+        tryMeButton = (Button) findViewById(R.id.tryMeButton);
+        tryMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                int randomNum = random.nextInt(colors.length);
+                windowView.setBackgroundColor(colors[randomNum]);
+                Log.d("Random", String.valueOf(randomNum));
+
+            }
+        });
     }
 }
